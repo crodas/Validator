1e59235b8dcd64c83eb453b1c06889841992e9f2
César D. Rodas <crodas@php.net>
2013-10-23 00:07:08 -0300

    composer.sh is better
    Now anyone can build the templates

--- compile.sh
+++ compile.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-view-compiler compile -N crodas\\Validator  $(pwd)/lib/crodas/Validator/Template $(pwd)/lib/crodas/Validator/Templates.php
+cd `dirname $0`
+
+php vendor/crodas/simple-view-engine/cli.php compile -N crodas\\Validator  $(pwd)/lib/crodas/Validator/Template $(pwd)/lib/crodas/Validator/Templates.php
